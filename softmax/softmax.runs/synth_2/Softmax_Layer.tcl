ce70b245e3ef1c62a2cc3ff6390d2022a6c7b81a
basantloay <basantloay2012@gmail.com>
2020-07-29 16:21:25 +0200

    Decreasing number of Luts by using division module once

--- softmax/softmax.runs/synth_2/Softmax_Layer.tcl
+++ softmax/softmax.runs/synth_2/Softmax_Layer.tcl
@@ -9,22 +9,21 @@ create_project -in_memory -part xc7a200tfbg676-2
 
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir D:/CNN2/softmax/softmax.cache/wt [current_project]
-set_property parent.project_path D:/CNN2/softmax/softmax.xpr [current_project]
+set_property webtalk.parent_dir D:/Softmax_CNN/softmax/softmax.cache/wt [current_project]
+set_property parent.project_path D:/Softmax_CNN/softmax/softmax.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property board_part xilinx.com:ac701:part0:1.0 [current_project]
 read_verilog -library xil_defaultlib {
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/mul.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/add.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/Xncalculation.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/term.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/reciporical.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/maxValue.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/exponential.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/Accumulator1.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/division.v
-  D:/CNN2/softmax/softmax.srcs/sources_1/new/Softmax_Layer.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/mul.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/add.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/Xncalculation.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/term.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/reciporical.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/maxValue.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/exponential.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/Accumulator1.v
+  D:/Softmax_CNN/softmax/softmax.srcs/sources_1/new/Softmax_Layer.v
 }
 synth_design -top Softmax_Layer -part xc7a200tfbg676-2
 write_checkpoint -noxdef Softmax_Layer.dcp
--- softmax/softmax.srcs/sources_1/new/Softmax_Layer.v
+++ softmax/softmax.srcs/sources_1/new/Softmax_Layer.v
@@ -12,9 +12,11 @@ output [31:0] chosenProbability;
 wire [319:0] exponentialArr1;
 wire [319:0] expoenetialArr2;
 wire [319:0] expoenetialArr3;
-wire [319:0] total;
-//reg [31:0] total1;
+//wire [319:0] total;
+wire [31:0] total1;
 wire [319:0] max1;
+wire[319:0] recp;
+localparam one=32'b00111111100000000000000000000000;
 genvar i;
 //calculate exponential for input array
 for (i=0;i<sizeArr;i=i+1)
@@ -32,22 +34,42 @@ Accumulator1
 )
 exponentialAddition(
 .Input_Arr(exponentialArr1),
-.Output(total[31:0])
+.Output(total1[31:0])
 );
 // total1=total;
-for (i=0;i<sizeArr-1;i=i+1)
+/*for (i=0;i<sizeArr-1;i=i+1)
 begin
 assign total[(i+1)*width+:width]= total[i*width+:width];
 end 
-
+*/
+reciporical r1(.numerator(one),
+                 .divisor(total1),
+                 .reciporical1(recp[31:0]));
+for (i=0;i<sizeArr-1;i=i+1)
+                 begin
+                 assign recp[(i+1)*width+:width]= recp[i*width+:width];
+                 end                  
 //apply division module to calculate probability in array
+
 for (i=0;i<sizeArr;i=i+1)
+begin
+mul div(.flp_a(expoenetialArr2[i*width+:width]),
+         .flp_b(recp[i*width+:width]),
+         .sign(outData[width*(i)+31]),
+         .exponent(outData[(width*i+30) -:8]),
+         .prod(outData[(width*i+22)-:23]));
+            
+assign expoenetialArr3[i*width+:width] = outData[i*width+:width];                        
+end
+
+/*for (i=0;i<sizeArr;i=i+1)
 begin
 division div(.dividend(expoenetialArr2[i*width+:width]),
              .divsor(total[i*width+:width]),
              .outDiv(outData[i*width+:width])); 
 assign expoenetialArr3[i*width+:width] = outData[i*width+:width];                        
 end
+*/
 
 //calculate maximum value in array
 assign max1[31:0]= expoenetialArr3[31:0];
